2bd423eba50571c4c23f58a7535945bf4a2ea899
Lukasz Maciejonczyk <lukasz.maciejonczyk@nordicsemi.no>
2020-12-21 14:29:03 +0100

    To be removed.
    Added for testing.
    
    Signed-off-by: Lukasz Maciejonczyk <lukasz.maciejonczyk@nordicsemi.no>

--- subsys/net/lib/openthread/platform/entropy.c
+++ subsys/net/lib/openthread/platform/entropy.c
@@ -4,9 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <kernel.h>
-#include <string.h>
-#include <drivers/entropy.h>
+#include <random/rand32.h>
 #include <logging/log.h>
 
 #include <openthread/platform/entropy.h>
@@ -15,32 +13,12 @@
 
 LOG_MODULE_REGISTER(net_otPlat_entropy, CONFIG_OPENTHREAD_L2_LOG_LEVEL);
 
-#if !defined(CONFIG_ENTROPY_HAS_DRIVER)
-#error OpenThread requires an entropy source for a TRNG
-#endif
-
 otError otPlatEntropyGet(uint8_t *aOutput, uint16_t aOutputLength)
 {
-	/* static to obtain it once in a first call */
-	static const struct device *dev;
-	int err;
-
-	if ((aOutput == NULL) || (aOutputLength == 0)) {
-		return OT_ERROR_INVALID_ARGS;
-	}
-
-	if (dev == NULL) {
-		dev = device_get_binding(DT_CHOSEN_ZEPHYR_ENTROPY_LABEL);
-		if (dev == NULL) {
-			LOG_ERR("Failed to obtain entropy device");
-			return OT_ERROR_FAILED;
-		}
-	}
-
-	err = entropy_get_entropy(dev, aOutput, aOutputLength);
-	if (err != 0) {
-		LOG_ERR("Failed to obtain entropy, err %d", err);
-		return OT_ERROR_FAILED;
+	if (IS_ENABLED(CONFIG_ENTROPY_HAS_DRIVER)) {
+		sys_csrand_get(aOutput, aOutputLength);
+	} else {
+		sys_rand_get(aOutput, aOutputLength);
 	}
 
 	return OT_ERROR_NONE;
